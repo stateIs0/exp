ad7ba255ce15c6849a0841195b52a63cd0a96e04
xuanmie <xuanmie@tsign.cn>
2023-12-05 16:09:16 +0800

    code opt

--- spring-adapter/open-exp-adapter-springboot1-starter/src/main/java/cn/think/in/java/open/exp/adapter/springboot1/RestUrlScanFactoryImpl.java
+++ spring-adapter/open-exp-adapter-springboot1-starter/src/main/java/cn/think/in/java/open/exp/adapter/springboot1/RestUrlScanFactoryImpl.java
@@ -1,43 +1,22 @@
 package cn.think.in.java.open.exp.adapter.springboot1;
 
+
+import lombok.extern.slf4j.Slf4j;
 import open.exp.rest.support.springboot.common.AloneRestUrlScan;
 import open.exp.rest.support.springboot.common.RestUrlScanFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
-/**
- * @version 1.0
- * @Author cxs
- * @Description
- * @date 2023/8/25
- **/
+@Slf4j
 public class RestUrlScanFactoryImpl implements RestUrlScanFactory {
-
-    static Logger log = LoggerFactory.getLogger(RestUrlScanFactoryImpl.class);
-
     @Override
-    public AloneRestUrlScan create(Object obj,
-                                   Object handlerMapping,
+    public AloneRestUrlScan create(Object obj, Object requestMappingHandlerMapping,
                                    Object handlerAdapter,
                                    String pluginId, Supplier<String> replaceEnabled) {
-        return new AloneRestUrlScanImpl(
-                (RequestMappingHandlerMapping) handlerMapping,
-                (RequestMappingHandlerAdapter) handlerAdapter,
-                obj,
-                replaceEnabled,
-                pluginId);
+        return new AloneRestUrlScanImpl(requestMappingHandlerMapping, handlerAdapter, obj, replaceEnabled, pluginId);
     }
 
     static class AloneRestUrlScanImpl implements AloneRestUrlScan {
@@ -45,17 +24,17 @@ public class RestUrlScanFactoryImpl implements RestUrlScanFactory {
         private final RequestMappingHandlerMapping handlerMapping;
         private final RequestMappingHandlerAdapter handlerAdapter;
         private final Object obj;
-        private List<RequestMappingInfoWrapper> scan;
-        private Supplier<String> replaceEnabled;
-        private String pluginId;
+        private List<ScanProcessor.RequestMappingInfoWrapper> requestMappingInfoWrappers;
+        private final Supplier<String> replaceEnabled;
+        private final String pluginId;
 
 
-        public AloneRestUrlScanImpl(RequestMappingHandlerMapping handlerMapping,
-                                    RequestMappingHandlerAdapter handlerAdapter,
+        public AloneRestUrlScanImpl(Object handlerMapping,
+                                    Object handlerAdapter,
                                     Object obj, Supplier<String> replaceEnabled,
                                     String pluginId) {
-            this.handlerMapping = handlerMapping;
-            this.handlerAdapter = handlerAdapter;
+            this.handlerMapping = (RequestMappingHandlerMapping) handlerMapping;
+            this.handlerAdapter = (RequestMappingHandlerAdapter) handlerAdapter;
             this.obj = obj;
             this.replaceEnabled = replaceEnabled;
             this.pluginId = pluginId;
@@ -63,8 +42,8 @@ public class RestUrlScanFactoryImpl implements RestUrlScanFactory {
 
         @Override
         public void register() {
-            scan = scan(obj.getClass());
-            for (RequestMappingInfoWrapper mapping : scan) {
+            requestMappingInfoWrappers = ScanProcessor.builder().build().scan(obj.getClass());
+            for (ScanProcessor.RequestMappingInfoWrapper mapping : requestMappingInfoWrappers) {
                 if (Boolean.parseBoolean(replaceEnabled.get())) {
                     // 覆盖, 删除老的 URL;
                     log.info("覆盖, 删除老的 URL {} {}", pluginId, (mapping.path));
@@ -81,115 +60,11 @@ public class RestUrlScanFactoryImpl implements RestUrlScanFactory {
 
         @Override
         public void unRegister() {
-            for (RequestMappingInfoWrapper mapping : scan) {
+            for (ScanProcessor.RequestMappingInfoWrapper mapping : requestMappingInfoWrappers) {
                 handlerMapping.unregisterMapping(mapping.requestMappingInfo);
                 log.warn("反注册 url, mapping = {}", (mapping.path));
             }
             handlerAdapter.afterPropertiesSet();
-        }
-
-        public List<RequestMappingInfoWrapper> scan(Class<?> aClass) {
-            List<RequestMappingInfoWrapper> result = new ArrayList<>();
-            Annotation[] annotations = aClass.getDeclaredAnnotations();
-            if (aClass.getName().contains("$$Enhancer")) {
-                // spring cglib
-                annotations = aClass.getSuperclass().getDeclaredAnnotations();
-                aClass = aClass.getSuperclass();
-            }
-            for (Annotation annotation : annotations) {
-                String[] parentPath = new String[]{""};
-                if (annotation.annotationType().equals(RestController.class) || annotation.annotationType().equals(Controller.class)) {
-                    //
-                    if (aClass.isAnnotationPresent(RequestMapping.class)) {
-                        RequestMapping mapping = aClass.getAnnotation(RequestMapping.class);
-                        parentPath = mapping.value();
-                    }
-
-                    Method[] declaredMethods = aClass.getDeclaredMethods();
-                    for (Method declaredMethod : declaredMethods) {
-                        boolean isPublic = Modifier.isPublic(declaredMethod.getModifiers());
-                        if (isPublic) {
-                            Annotation[] an = declaredMethod.getDeclaredAnnotations();
-                            for (Annotation ann : an) {
-                                if (ann.annotationType().equals(RequestMapping.class)) {
-                                    RequestMapping mapping = declaredMethod.getAnnotation(RequestMapping.class);
-                                    RequestMethod[] method = mapping.method();
-                                    String path = RequestMappingInfoWrapper.buildPath(parentPath, mapping.value());
-                                    result.add(new RequestMappingInfoWrapper(RequestMappingInfo
-                                            .paths(path) // 设置URL路径
-                                            .methods(method) // 设置HTTP方法
-                                            .params(mapping.params())
-                                            .headers(mapping.headers())
-                                            .build(), declaredMethod, path));
-                                }
-                                if (ann.annotationType().equals(GetMapping.class)) {
-                                    GetMapping mapping = declaredMethod.getAnnotation(GetMapping.class);
-                                    String path = RequestMappingInfoWrapper.buildPath(parentPath, mapping.value());
-                                    result.add(new RequestMappingInfoWrapper(RequestMappingInfo
-                                            .paths(path) // 设置URL路径
-                                            .methods(RequestMethod.GET) // 设置HTTP方法
-                                            .params(mapping.params())
-                                            .headers(mapping.headers())
-                                            .build(), declaredMethod, path));
-                                }
-
-                                if (ann.annotationType().equals(PostMapping.class)) {
-                                    PostMapping mapping = declaredMethod.getAnnotation(PostMapping.class);
-                                    String path = RequestMappingInfoWrapper.buildPath(parentPath, mapping.value());
-                                    result.add(new RequestMappingInfoWrapper(RequestMappingInfo
-                                            .paths(path) // 设置URL路径
-                                            .methods(RequestMethod.POST) // 设置HTTP方法
-                                            .params(mapping.params())
-                                            .headers(mapping.headers())
-                                            .build(), declaredMethod, path));
-                                }
-
-                                if (ann.annotationType().equals(PutMapping.class)) {
-                                    PutMapping mapping = declaredMethod.getAnnotation(PutMapping.class);
-                                    String path = RequestMappingInfoWrapper.buildPath(parentPath, mapping.value());
-                                    result.add(new RequestMappingInfoWrapper(RequestMappingInfo
-                                            .paths(path) // 设置URL路径
-                                            .methods(RequestMethod.PUT) // 设置HTTP方法
-                                            .params(mapping.params())
-                                            .headers(mapping.headers())
-                                            .build(), declaredMethod, path));
-                                }
-
-                                if (ann.annotationType().equals(DeleteMapping.class)) {
-                                    DeleteMapping mapping = declaredMethod.getAnnotation(DeleteMapping.class);
-                                    String path = RequestMappingInfoWrapper.buildPath(parentPath, mapping.value());
-                                    result.add(new RequestMappingInfoWrapper(RequestMappingInfo
-                                            .paths(path) // 设置URL路径
-                                            .methods(RequestMethod.DELETE) // 设置HTTP方法
-                                            .params(mapping.params())
-                                            .headers(mapping.headers())
-                                            .build(), declaredMethod, path));
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return result;
-        }
-    }
-
-    static class RequestMappingInfoWrapper {
-        public RequestMappingInfo requestMappingInfo;
-        public Method method;
-        public String path;
-
-        public RequestMappingInfoWrapper(RequestMappingInfo requestMappingInfo, Method method, String path) {
-            this.requestMappingInfo = requestMappingInfo;
-            this.method = method;
-            this.path = path;
-        }
-
-        public static String buildPath(String[] parent, String[] subPath) {
-            if (subPath[0].startsWith("/")) {
-                return parent[0] + subPath[0];
-            }
-            return parent[0] + "/" + subPath[0];
         }
     }
 }
---
+++ spring-adapter/open-exp-adapter-springboot2-starter/src/main/java/cn/think/in/java/open/exp/adapter/springboot2/ScanProcessor.java
@@ -0,0 +1,196 @@
+package cn.think.in.java.open.exp.adapter.springboot2;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Builder
+public class ScanProcessor {
+
+    private final List<Handler> list = new CopyOnWriteArrayList<>();
+
+    {
+        list.add(new RequestMappingHandler());
+        list.add(new GetMappingHandler());
+        list.add(new PostMappingHandler());
+        list.add(new DeleteMappingHandler());
+        list.add(new PutMappingHandler());
+    }
+
+    public List<RequestMappingInfoWrapper> scan(Class<?> aClass) {
+        List<RequestMappingInfoWrapper> result = new ArrayList<>();
+        Annotation[] annotations = aClass.getDeclaredAnnotations();
+        if (aClass.getName().contains("$$EnhancerBySpringCGLIB$$")) {
+            // spring cglib
+            annotations = aClass.getSuperclass().getDeclaredAnnotations();
+            aClass = aClass.getSuperclass();
+        }
+
+        for (Annotation annotation : annotations) {
+            processAnnotation(aClass, annotation, result);
+        }
+
+        return result;
+    }
+
+    private void processAnnotation(Class<?> aClass, Annotation annotation, List<RequestMappingInfoWrapper> result) {
+        String[] parentPath = new String[]{""};
+        if (annotation.annotationType().equals(RestController.class) || annotation.annotationType().equals(Controller.class)) {
+            if (aClass.isAnnotationPresent(RequestMapping.class)) {
+                RequestMapping mapping = aClass.getAnnotation(RequestMapping.class);
+                parentPath = mapping.value().length == 0 ? mapping.path() : mapping.value();
+            }
+            Method[] declaredMethods = aClass.getDeclaredMethods();
+            for (Method declaredMethod : declaredMethods) {
+                doScan(declaredMethod, result, parentPath);
+            }
+        }
+    }
+
+    private void doScan(Method declaredMethod,
+                        List<RequestMappingInfoWrapper> resultList,
+                        String[] parentPath) {
+
+        for (Handler handler : list) {
+            Optional<Result> result = handler.handler(declaredMethod);
+            result.ifPresent(r -> {
+                RequestMethod[] methods = r.methods;
+                String[] params = r.params;
+                String[] headers = r.headers;
+                String[] path = r.path;
+                String pathFinal = RequestMappingInfoWrapper.buildPath(parentPath, path);
+                resultList.add(new RequestMappingInfoWrapper(RequestMappingInfo
+                        .paths(pathFinal)
+                        .methods(methods)
+                        .params(params)
+                        .headers(headers)
+                        .build(), declaredMethod, pathFinal));
+
+            });
+        }
+
+    }
+
+
+    @Builder
+    @Data
+    static class Result {
+        RequestMethod[] methods;
+        String[] params;
+        String[] headers;
+        String[] path;
+    }
+
+    interface Handler {
+        Optional<Result> handler(Method declaredMethod);
+    }
+
+    public static class RequestMappingInfoWrapper {
+        public RequestMappingInfo requestMappingInfo;
+        public Method method;
+        public String path;
+
+        public RequestMappingInfoWrapper(RequestMappingInfo requestMappingInfo, Method method, String path) {
+            this.requestMappingInfo = requestMappingInfo;
+            this.method = method;
+            this.path = path;
+        }
+
+        public static String buildPath(String[] parent, String[] subPath) {
+            if (subPath.length == 0) {
+                subPath = new String[]{"/"};
+            }
+            if (parent.length == 0) {
+                parent = new String[]{"/"};
+            }
+            if (subPath[0].startsWith("/")) {
+                return parent[0] + subPath[0];
+            }
+            return parent[0] + "/" + subPath[0];
+        }
+    }
+
+    private static Optional<Result> getResult(RequestMethod[] methods, String[] params, String[] headers, String[] value, String[] path2) {
+        String[] path = value.length == 0 ? path2 : value;
+        return Optional.of(Result.builder()
+                .methods(methods)
+                .params(params)
+                .headers(headers)
+                .path(path)
+                .build());
+    }
+
+    static class RequestMappingHandler implements Handler {
+
+        @Override
+        public Optional<Result> handler(Method declaredMethod) {
+            RequestMapping mapping = declaredMethod.getAnnotation(RequestMapping.class);
+            if (mapping != null) {
+                RequestMethod[] methods = mapping.method();
+                return getResult(methods, mapping.params(), mapping.headers(), mapping.value(), mapping.path());
+            }
+            return Optional.empty();
+        }
+    }
+
+    static class GetMappingHandler implements Handler {
+
+        @Override
+        public Optional<Result> handler(Method declaredMethod) {
+            GetMapping mapping = declaredMethod.getAnnotation(GetMapping.class);
+            if (mapping != null) {
+                RequestMethod[] methods = new RequestMethod[]{RequestMethod.GET};
+                return getResult(methods, mapping.params(), mapping.headers(), mapping.value(), mapping.path());
+            }
+            return Optional.empty();
+        }
+    }
+
+    static class PostMappingHandler implements Handler {
+
+        @Override
+        public Optional<Result> handler(Method declaredMethod) {
+            PostMapping mapping = declaredMethod.getAnnotation(PostMapping.class);
+            if (mapping != null) {
+                RequestMethod[] methods = new RequestMethod[]{RequestMethod.POST};
+                return getResult(methods, mapping.params(), mapping.headers(), mapping.value(), mapping.path());
+            }
+            return Optional.empty();
+        }
+    }
+
+    static class DeleteMappingHandler implements Handler {
+
+        @Override
+        public Optional<Result> handler(Method declaredMethod) {
+            DeleteMapping mapping = declaredMethod.getAnnotation(DeleteMapping.class);
+            if (mapping != null) {
+                RequestMethod[] methods = new RequestMethod[]{RequestMethod.DELETE};
+                return getResult(methods, mapping.params(), mapping.headers(), mapping.value(), mapping.path());
+            }
+            return Optional.empty();
+        }
+    }
+
+    static class PutMappingHandler implements Handler {
+
+        @Override
+        public Optional<Result> handler(Method declaredMethod) {
+            PutMapping mapping = declaredMethod.getAnnotation(PutMapping.class);
+            if (mapping != null) {
+                RequestMethod[] methods = new RequestMethod[]{RequestMethod.DELETE};
+                return getResult(methods, mapping.params(), mapping.headers(), mapping.value(), mapping.path());
+            }
+            return Optional.empty();
+        }
+    }
+}
