8b05862006986818555eb09688d1d094ee09ef45
xuanmie <xuanmie@tsign.cn>
2023-12-13 11:45:56 +0800

    fix bug 12-13

--- spring-adapter/open-exp-adapter-springboot27-starter/src/main/java/cn/think/in/java/open/exp/adapter/springboot27/ScanProcessor.java
+++ spring-adapter/open-exp-adapter-springboot27-starter/src/main/java/cn/think/in/java/open/exp/adapter/springboot27/ScanProcessor.java
@@ -29,7 +29,7 @@ public class ScanProcessor {
     public List<RequestMappingInfoWrapper> scan(Class<?> aClass) {
         List<RequestMappingInfoWrapper> result = new ArrayList<>();
         Annotation[] annotations = aClass.getDeclaredAnnotations();
-        if (aClass.getName().contains("$$EnhancerBySpringCGLIB$$")) {
+        if (aClass.getName().contains("$$Enhancer")) {
             // spring cglib
             annotations = aClass.getSuperclass().getDeclaredAnnotations();
             aClass = aClass.getSuperclass();
